c019115ec8182cd708463e71f6c167ab72dbd106
Azar Imranov <imranov@ut.ee>
2024-10-11 17:50:42 +0300

    DB connection configs

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,21 +1,21 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
-import { Post } from "./entity/Post"
-import { User } from "./entity/User"
-import { Comment } from "./entity/Comment"
+import { Post } from "./entity/Post";
+import { User } from "./entity/User";
+import { Comment } from "./entity/Comment";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "postgres",
-    entities: [Post , User ,Comment],
-    synchronize: true,
-    logging: false,
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "imranovazer",
+  password: "20023838Aze",
+  database: "postgres",
+  entities: [Post, User, Comment],
+  synchronize: true,
+  logging: false,
+});
 
 // to initialize the initial connection with the database, register all entities
 // and "synchronize" database schema, call "initialize()" method of a newly created database
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,51 +1,57 @@
-
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, BaseEntity, ManyToMany, JoinTable } from "typeorm"
-import bcrypt from 'bcrypt'
-import { Post } from "./Post"
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  BaseEntity,
+  ManyToMany,
+  JoinTable,
+} from "typeorm";
+import bcrypt from "bcrypt";
+import { Post } from "./Post";
 
 @Entity()
-export class User  extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    username: string 
+  @Column()
+  username: string;
 
-    @Column()
-    email: string
-    @Column()
-    password : string 
+  @Column()
+  email: string;
+  @Column()
+  password: string;
 
-    @ManyToMany(() => User, (user) => user.following 
-    
+  @ManyToMany(
+    () => User,
+    (user) => user.following
+
     // ,{cascade:true}
-    )
-   
-    followers: User[]
+  )
+  followers: User[];
 
+  @ManyToMany(
+    () => User,
+    (user) => user.followers
 
-    @ManyToMany(() => User, (user) => user.followers 
-    
     // ,{cascade:true}
-    )
-    @JoinTable()
-    following: User[]
+  )
+  @JoinTable()
+  following: User[];
 
+  @ManyToMany(() => Post, (post) => post.likes, { cascade: true })
+  @JoinTable()
+  favoritePosts: Post[];
 
-    @ManyToMany(() => Post , post=>post.likes,{cascade:true}
-    )
-    @JoinTable()
-    favoritePosts: Post[]
+  @OneToMany(() => Post, (post) => post.author, { cascade: true })
+  posts: Post[];
 
-    @OneToMany(()=>Post , (post)=>post.author 
-    , { cascade: true }
-    ) 
-    posts:Post[] 
-
-    public async correctPassword( candidatePassword:string ,userPassword:string) : Promise<boolean>{
-
-      console.log('You are here ' , candidatePassword ,userPassword)
-        return await bcrypt.compare(candidatePassword, userPassword);
-  };
-
+  public async correctPassword(
+    candidatePassword: string,
+    userPassword: string
+  ): Promise<boolean> {
+    console.log("You are here ", candidatePassword, userPassword);
+    return await bcrypt.compare(candidatePassword, userPassword);
+  }
 }
